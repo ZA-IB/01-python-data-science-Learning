88eff977bb1715d0af6a9fb2fc8482b2eb3734db
zaib-786-noob <jehanzeb7@outlook.com>
2025-01-05 09:12:50 +0500

    Add files via upload

---
+++ 02_operators.py
@@ -0,0 +1,14 @@
+print("2+2")
+print("3-2")
+print("2*3")
+print("8/2")
+print("9/2")
+
+print(2+2)
+print(3-2)
+print(2*3)
+print(8/2)
+print(9/2)
+print(9//2) #divide but with out point
+print(2**3) #power
+print(3**2/2*3/3+6-5) #BODMAS rule , B: Brackets O: Order (or powers or roots) D: Division M: Multiplication A: Addition S: Subtraction
