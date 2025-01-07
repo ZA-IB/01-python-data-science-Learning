fe4b30af4e399b7062c713c839498069dc0a6a8b
ZA-IB <jehanzeb7@outlook.com>
2025-01-08 02:41:25 +0500

    Add files via upload

---
+++ 06_input_variables.py
@@ -0,0 +1,20 @@
+# fruit_basket = "Mangoes"
+# print(fruit_basket)
+
+# input function
+# fruit_basket = input("what is your fav fruit?")
+# print(fruit_basket)
+
+# input function of 2rd stage
+# name = input ("what is your name?")
+# greetings = "Hello!"
+
+# print(greetings,name)
+
+# input function of 3rd stage
+name = input ("what is your name?")
+age = input ("how old are you?")
+greetings = "Hello!"
+
+print(greetings,name, "you are still young")
+
---
+++ 07_conditional_logics.py
@@ -0,0 +1,32 @@
+# logical operaters
+# are either true false yes no 0 1
+
+# eual to                       ==
+# less than                     <
+# greater than                  >
+# less than and equal to        <=
+# grater than and equal to      >=
+# not equal to                  !=
+
+print(4==4)
+print(4!=4)
+print(4>=4)
+print(4<=4)
+print(4<3)
+print(4>3)
+
+# application of logical operators
+
+hammad_age=4
+age_at_school=5
+
+print (hammad_age==age_at_school)
+
+# input operator and logics
+
+age_at_school = 5
+hammad_age=input("how old is hammad?")
+hammad_age=int(hammad_age)
+print(hammad_age>=age_at_school,)
+
+
